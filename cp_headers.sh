f441e54c776e4a17bdb2a836c9ab779964ed9282
Pavel Dubrova <pashadubrova@gmail.com>
2026-04-17 11:35:09 +0300

    cp_headers: Update headers location according to 6.6 changes
    Some of the UAPI headers were moved into techpack packages.
    Update all the header lists to reflect this.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- cp_headers.sh
+++ cp_headers.sh
@@ -19,17 +19,12 @@ UAPI_HEADERS="\
     linux/fb.h\
     linux/fuse.h\
     linux/ion.h\
-    linux/ipa_qmi_service_v01.h\
     linux/msm_ion.h\
     linux/msm_ion_ids.h\
-    linux/msm_ipa.h\
     linux/msm_rmnet.h\
     linux/netlink.h\
     linux/qseecom.h\
-    linux/rmnet_data.h\
-    linux/rmnet_ipa_fd_ioctl.h\
     linux/socket.h\
-    linux/spcom.h\
     linux/v4l2-common.h\
     linux/v4l2-controls.h\
     linux/v4l2-mediabus.h\
@@ -41,7 +36,7 @@ UAPI_HEADERS="\
 
 TECHPACK_AUDIO_UAPI_HEADERS="\
     linux/msm_audio.h\
-    sound/audio_compressed_formats.h\
+    linux/msm_audio_calibration.h\
     sound/audio_effects.h\
     sound/audio_slimslave.h\
     sound/devdep_params.h\
@@ -70,20 +65,56 @@ TECHPACK_CAMERA_UAPI_HEADERS="\
     media/cam_sync.h\
     media/cam_tfe.h"
 
+TECHPACK_DATAIPA_UAPI_HEADERS="\
+    linux/ipa_qmi_service_v01.h\
+    linux/msm_ipa.h\
+    linux/rmnet_ipa_fd_ioctl.h"
+
 TECHPACK_DISPLAY_UAPI_HEADERS="\
+    display/drm/msm_drm_aiqe.h\
     display/drm/msm_drm_pp.h\
     display/drm/sde_drm.h\
+    display/hdcp/msm_hdmi_hdcp_mgr.h\
     display/media/mmm_color_fmt.h\
     display/media/msm_sde_rotator.h"
+
+TECHPACK_DSP_UAPI_HEADERS="\
+    misc/fastrpc.h"
 
 TECHPACK_GRAPHICS_UAPI_HEADERS="\
     linux/msm_kgsl.h"
 
 TECHPACK_SECUREMSM_UAPI_HEADERS="\
+    linux/compat_qcedev.h\
+    linux/qcedev.h\
+    linux/qcota.h\
+    linux/qrng.h\
+    linux/qseecom.h\
+    linux/qseecom_32bit_impl.h\
+    linux/qseecom_api.h\
     linux/smcinvoke.h"
 
-TECHPACK_VIDEO_UAPI_HEADERS="\
+TECHPACK_SMMU_PROXY_UAPI_HEADERS="\
+    linux/qti-smmu-proxy.h"
+
+TECHPACK_SPU_UAPI_HEADERS="\
+    linux/spcom.h\
+    linux/spss_utils.h"
+
+TECHPACK_SYNC_FENCE_UAPI_HEADERS="\
+    sync_fence/qcom_sync_file.h"
+
+TECHPACK_SYNX_UAPI_HEADERS="\
+    synx/media/synx_header.h"
+
+TECHPACK_UBWCP_UAPI_HEADERS="\
+    ubwcp_ioctl.h"
+
+TECHPACK_VIDEO_HEADERS="\
     msm_media_info.h"
+
+TECHPACK_VIDEO_UAPI_HEADERS="\
+    vidc/media/v4l2_vidc_extensions.h"
 
 HEADER_OVERRIDES="\
     display/media/mmm_color_fmt.h\
@@ -117,13 +148,29 @@ process_headers "$TECHPACK_AUDIO_UAPI_HEADERS" "../techpack/audio/include/uapi/a
 
 process_headers "$TECHPACK_CAMERA_UAPI_HEADERS" "../techpack/camera/include/uapi/camera/" ""
 
+process_headers "$TECHPACK_DATAIPA_UAPI_HEADERS" "../techpack/dataipa/drivers/platform/msm/include/uapi/" ""
+
 process_headers "$TECHPACK_DISPLAY_UAPI_HEADERS" "../techpack/display/include/uapi/" ""
+
+process_headers "$TECHPACK_DSP_UAPI_HEADERS" "../techpack/dsp/include/uapi/" ""
 
 process_headers "$TECHPACK_GRAPHICS_UAPI_HEADERS" "../techpack/graphics/include/uapi/" ""
 
-process_headers "$TECHPACK_SECUREMSM_UAPI_HEADERS" "../techpack/securemsm/" ""
+process_headers "$TECHPACK_SECUREMSM_UAPI_HEADERS" "../techpack/securemsm/include/uapi/" ""
 
-process_headers "$TECHPACK_VIDEO_UAPI_HEADERS" "../techpack/video/driver/vidc/inc/" "media/"
+process_headers "$TECHPACK_SMMU_PROXY_UAPI_HEADERS" "../techpack/securemsm/smmu-proxy/include/uapi/" ""
+
+process_headers "$TECHPACK_SPU_UAPI_HEADERS" "../techpack/spu/include/uapi/" ""
+
+process_headers "$TECHPACK_SYNC_FENCE_UAPI_HEADERS" "../techpack/mm/sync_fence/include/uapi/" ""
+
+process_headers "$TECHPACK_SYNX_UAPI_HEADERS" "../techpack/synx/include/uapi/" ""
+
+process_headers "$TECHPACK_UBWCP_UAPI_HEADERS" "../techpack/mm-sys/ubwcp/include/uapi/" ""
+
+# Some components require these internal headers
+process_headers "$TECHPACK_VIDEO_HEADERS" "../techpack/video/driver/vidc/inc/" "media/"
+process_headers "$TECHPACK_VIDEO_UAPI_HEADERS" "../techpack/video/include/uapi/" ""
 
 for x in $HEADER_OVERRIDES; do \
     cp $HEADER_ORI/$x $HEADER_SAN/$x
